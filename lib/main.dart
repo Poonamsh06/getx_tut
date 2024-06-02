7377a9c2e5cb719448ea30d294f9fd5f5b745a0d
unknown <poonamsh061002@gmail.com>
2024-06-02 13:04:03 +0530

    favourite and switch with Getx  statemanagement

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_tut/example_two.dart';
+import 'package:getx_tut/favourite.dart';
 import 'package:getx_tut/home.dart';
 import 'package:getx_tut/localization.dart';
 import 'package:getx_tut/services/translation_services.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: SwitchPage()
+        home: FavouritePage()
         // LocalizationPage(),
         );
   }
